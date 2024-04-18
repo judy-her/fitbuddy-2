7cd376b563bed163aa02b293fab56546e4396df9
judy-her <judith85dm@ucla.edu>
2024-04-17 20:11:09 -0700

    added queries for with gql in server-utils-queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,80 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
+// Define QUERY_USER query using gql
 export const QUERY_USER = gql`
   query user {
     user {
+      _id
       username
       email
-      bookCount
-      savedBooks {
-        authors
-        description
-        bookId
+      password
+      savedExercises {
+        _id
         title
+        description
+        exerciseId
+        equipment
         image
-        link
+        category {
+          _id
+          name
+        }
+      }
+      exerciseCount
+    }
+  }
+}
+`;
+
+// Define QUERY_EXERCISE query using gql
+export const QUERY_EXERCISE = gql`
+  query exercise($_id: ID!) {
+    exercise(_id: $_id) {
+      _id
+      title
+      description
+      exerciseId
+      equipment
+      image
+      category {
+        _id
+        name
       }
     }
   }
 `;
+
+// Define QUERY_CATEGORY query using gql
+export const QUERY_CATEGORY = gql`
+  query category($_id: ID!) {
+    category(_id: $_id) {
+      _id
+      name
+      exercises {
+        _id
+        title
+        description
+        exerciseId
+        equipment
+        image
+      }
+    }
+  }
+`;
+// export const QUERY_USER = gql`
+//   query user {
+//     user {
+//       username
+//       email
+//       bookCount
+//       savedBooks {
+//         authors
+//         description
+//         bookId
+//         title
+//         image
+//         link
+//       }
+//     }
+//   }
+// `;
