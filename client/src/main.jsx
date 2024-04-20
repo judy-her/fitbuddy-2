76734d8731b47b51d5df633be7c2bc94bf763e07
judy-her <judith85dm@ucla.edu>
2024-04-19 21:30:39 -0700

    changed home page to be main, and added Home page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,34 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App.jsx'
-import SearchExercises from './pages/SearchExercises.jsx'
-import SavedExercises from './pages/SavedExercises.jsx'
+import App from './App.jsx';
+import SearchExercises from './pages/SearchExercises.jsx';
+import SavedExercises from './pages/SavedExercises.jsx';
+import Home from './pages/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
     children: [
       {
         index: true,
-        element: <SearchExercises />
-      }, {
+        element: <Home />,
+      },
+      {
+        path: '/search',
+        element: <SearchExercises />,
+      },
+      {
         path: '/saved',
-        element: <SavedExercises />
-      }
-    ]
-  }
-])
+        element: <SavedExercises />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
