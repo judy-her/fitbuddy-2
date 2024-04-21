cb637f8691988b10ea52a21cd354db4d1bc4fc43
judy-her <judith85dm@ucla.edu>
2024-04-21 11:55:20 -0700

    logout bug fixed

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -45,7 +45,7 @@ class AuthService {
 
   logout() {
     // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
+    sessionStorage.removeItem('id_token');
     // this will reload the page and reset the state of the application
     window.location.assign('/');
   }
