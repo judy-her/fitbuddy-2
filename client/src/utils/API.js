c73ab52371ae3958d11c83474dde932e666be62a
judy-her <judith85dm@ucla.edu>
2024-04-21 20:00:32 -0700

    changed api key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -60,7 +60,7 @@ export const fetchCategories = () => {
   return fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '512ee0aacamsh7a407feedf48123p1b1ac8jsn48d2ae56ac6f',
+      'X-RapidAPI-Key': '68788e6872msh6d6e3e4429b17c2p110f25jsnb67f55d7b577',
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     },
   });
@@ -72,7 +72,7 @@ export const getExercises = (selectedCategory) => {
     {
       method: 'GET',
       headers: {
-        'X-RapidAPI-Key': '512ee0aacamsh7a407feedf48123p1b1ac8jsn48d2ae56ac6f',
+        'X-RapidAPI-Key': '68788e6872msh6d6e3e4429b17c2p110f25jsnb67f55d7b577',
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
       },
     }
@@ -86,7 +86,7 @@ export const getExercise = (exerciseId) => {
     {
       method: 'GET',
       headers: {
-        'X-RapidAPI-Key': '512ee0aacamsh7a407feedf48123p1b1ac8jsn48d2ae56ac6f',
+        'X-RapidAPI-Key': '68788e6872msh6d6e3e4429b17c2p110f25jsnb67f55d7b577',
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
       },
     }
